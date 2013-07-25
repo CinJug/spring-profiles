36981144d77cca52551e82b16d5aeb7a17d60b4f
Trey Howard <trey.howard@gmail.com>
2013-07-25 08:46:38 -0400

    Refactored spring bootstrap test to verify all environments programmatically. Formerly required spring.profiles.active property to be provided when running unit tests.

--- src/test/java/TestSpringBootstrap.java
+++ src/test/java/TestSpringBootstrap.java
@@ -8,8 +8,20 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class TestSpringBootstrap {
 
+	@Test(expected = Exception.class)
+	public void test_Missing() {
+		new ClassPathXmlApplicationContext("spring.xml");
+	}
+
 	@Test
-	public void test_LoadContext() {
+	public void test_Workstation() {
+		System.setProperty("spring.profiles.active", "workstation");
+		new ClassPathXmlApplicationContext("spring.xml");
+	}
+
+	@Test
+	public void test_Dev() {
+		System.setProperty("spring.profiles.active", "dev");
 		new ClassPathXmlApplicationContext("spring.xml");
 	}
 
